897e7a4d3310e2708dbdbf34ad5653d32a011738
vp <vp@gmail.com>
2014-10-19 16:04:59 +0300

    L3:Specifying Literal Text

--- deitel/TipCalculatorMy/gen/com/vp/tipcalculatormy/R.java
+++ deitel/TipCalculatorMy/gen/com/vp/tipcalculatormy/R.java
@@ -52,8 +52,14 @@ public final class R {
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
+        public static final int amount=0x7f050003;
         public static final int app_name=0x7f050000;
+        public static final int custom_tip_percenteg=0x7f050004;
+        public static final int eighteen_percent=0x7f050008;
+        public static final int fifteen_percent=0x7f050007;
         public static final int hello_world=0x7f050001;
+        public static final int tip=0x7f050005;
+        public static final int total=0x7f050006;
     }
     public static final class style {
         /** 
